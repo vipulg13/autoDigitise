0b7867dd615f34e068db2a1004b92770ea9b9a88
Gupta <vipul.gupta@hereon.de>
2024-05-16 01:32:01 +0200

    removing the dependency of white axis calculation and general improvements

--- find_xy_px_coords.R
+++ find_xy_px_coords.R
@@ -7,6 +7,7 @@ library(EBImage)
 # input: an EBImage object
 # output: a list containing coordinate values (width, height) of x1, x2, y1, y2  
 getXYCoordinates <- function(img) {
+  
   # find the coordinates of axes intersection point (x1 and y1)
   img_gray <- channel(img, "gray")
   img_gray <- img_gray > 0.8
@@ -16,70 +17,96 @@ getXYCoordinates <- function(img) {
   px_fact <- ifelse(px_fact == 0, 1, px_fact)
   sum_row <- apply(img_gray, 1, sum)
   sum_row[round(width/2):width] <- NA
-  x_axis_start <- which.min(sum_row)
+  axes_intersect_width <- which.min(sum_row)
   
   # logic to make decision for axis by checking 
   # the presence of the sequence of black pixels
   for (w in 1:(round(width/2)-1)) {
-    p_x_start <- which(img_gray[w,] == FALSE)
-    if (length(p_x_start)) {
-      seq_black_px <- rle(diff(p_x_start))$lengths
-      if (length(seq_black_px) && max(seq_black_px) > height/5) {
-        if (abs(x_axis_start-w) > 4*px_fact) {
-          x_axis_start <- w
-        } else {
-          if (px_fact > 1)
-            x_axis_start <- w + 2*(px_fact-(1/2))
+    p_axes_intersect_width <- which(img_gray[w,] == FALSE)
+    if (length(p_axes_intersect_width)) {
+      seq_black_px <- rle(diff(p_axes_intersect_width))$lengths
+      if (length(seq_black_px) && max(seq_black_px) > height/4) {
+        if (abs(axes_intersect_width-w) > 4*px_fact) {
+          axes_intersect_width <- w
         }
-        break()
+        if (px_fact > 1) {
+          axes_intersect_width <- w + 2*(px_fact-(1/2))
+        }
+        
+        # find the y2 height
+        y_black_px <- which(img_gray[axes_intersect_width,] == FALSE)
+        rle_y <- rle(diff(y_black_px))
+        y_seq_indices <- which(rle_y$values == 1)
+        max_value_y_index <- which.max(rle_y$lengths[y_seq_indices])
+        max_seq_y_index <- y_seq_indices[max_value_y_index]
+        cum_sum_y_index <- cumsum(rle_y$lengths)
+        y_axis_end_height_index <- ifelse(max_seq_y_index == 1, 1, cum_sum_y_index[max_seq_y_index-1]+1)
+        y_axis_end_height <- y_black_px[y_axis_end_height_index] - 1 + 2*(px_fact-(1/2))
+        break
       }
     }
   }
   sum_column <- apply(img_gray, 2, sum)
   sum_column[1:round(height/2)] <- NA
-  y_axis_start <- which.min(sum_column)
+  axes_intersect_height <- which.min(sum_column)
   
   # this supports the cases where the logarithmic ticks are span 
   # over the plot, which hallucinates the algorithm to select the x axis
   for (h in height:(round(height/2))) {
-    p_y_start <- which(img_gray[,h] == FALSE)
-    if (length(p_y_start)) {
-      seq_black_px <- rle(diff(p_y_start))$lengths
+    p_axes_intersection_height <- which(img_gray[,h] == FALSE)
+    if (length(p_axes_intersection_height)) {
+      seq_black_px <- rle(diff(p_axes_intersection_height))$lengths
       
       # check if a line-like object is detected
-      if (length(seq_black_px) && max(seq_black_px) > width/5) {
+      if (length(seq_black_px) && max(seq_black_px) > width/4) {
         
         # check the proximity of the detected line object with the initial line object
-        if (abs(y_axis_start-h) > 4*px_fact) {
-          y_axis_start <- h
-        } else {
-          
-          # find the center point of the line object in cases when pixel factor is more than 1
-          if (px_fact > 1)
-            y_axis_start <- h - 2*(px_fact-(1/2))
+        if (abs(axes_intersect_height-h) > 4*px_fact) {
+          axes_intersect_height <- h #- 2*(px_fact-(1/2)) 
         }
-        break()
+        
+        # find the center point of the line object in cases when pixel factor is more than 1
+        if (px_fact > 1) {
+          axes_intersect_height <- h - 2*(px_fact-(1/2)) 
+        }
+        
+        # find the x2 width
+        x_black_px <- which(img_gray[,axes_intersect_height] == FALSE)
+        rle_x <- rle(diff(x_black_px))
+        x_seq_indices <- which(rle_x$values == 1)
+        max_value_x_index <- which.max(rle_x$lengths[x_seq_indices])
+        max_seq_x_index <- x_seq_indices[max_value_x_index]
+        cum_sum_x_index <- cumsum(rle_x$lengths)
+        x_axis_end_width_index <- ifelse(max_seq_x_index == length(rle_x$lengths), length(x_black_px), cum_sum_x_index[max_seq_x_index-1])
+        x_axis_end_width <-  x_black_px[x_axis_end_width_index] + 1 - 2*(px_fact-(1/2))
+        break
       }
     }
   }
   
-  # find where x and y axes end
-  white_y_indices <- which(img_gray[x_axis_start,] == TRUE)
-  p_y_axis_end <- white_y_indices[white_y_indices < y_axis_start & white_y_indices < height/3]
-  y_axis_end <- 1
-  if (length(p_y_axis_end))
-    y_axis_end <- max(p_y_axis_end) + 1
-  white_x_indices <- which(img_gray[, y_axis_start] == TRUE)
-  p_x_axis_end <- white_x_indices[white_x_indices > x_axis_start & white_x_indices > 2*(width/3)]
-  x_axis_end <- width
-  if (length(p_x_axis_end))
-    x_axis_end <- min(p_x_axis_end) - 1
-  initAxesVec <- c(x_axis_start, x_axis_end, y_axis_start, y_axis_end)
+  # special case: when axes are not intersecting
+  # if (img_gray[axes_intersect_width,axes_intersect_height]) {
+  #   x_axis_start_width_index <- ifelse(max_seq_x_index == 1, 1, cum_sum_x_index[max_seq_x_index-1]) + 1
+  #   axes_intersect_width <- x_black_px[x_axis_start_width_index] + 1 - 2*(px_fact-(1/2))
+  #   y_axis_start_height_index <- ifelse(max_seq_y_index == 1, rle_y$lengths+1, cum_sum_y_index[max_seq_y_index]+1)
+  #   axes_intersect_height <- y_black_px[y_axis_start_height_index] - 1 + 2*(px_fact-(1/2))
+  # }
+  
+  
+  initAxesVec <- c(axes_intersect_width, x_axis_end_width, axes_intersect_height, y_axis_end_height)
+  if (any(is.na(initAxesVec)) || !length(initAxesVec) == 4)
+    stop("all initial axes points could not be located")
   lst <- list()
   lst$x1_coord <- locateXCoordinates(img_gray, initAxesVec, axis_pos = "start", px_fact = px_fact)
   lst$x2_coord <- locateXCoordinates(img_gray, initAxesVec, axis_pos = "end", px_fact = px_fact)
   lst$y1_coord <- locateYCoordinates(img_gray, initAxesVec, axis_pos = "start", px_fact = px_fact)
   lst$y2_coord <- locateYCoordinates(img_gray, initAxesVec, axis_pos = "end", px_fact = px_fact)
+  
+  # special case: when axes are not intersecting
+  if (all(img_gray[lst$x1_coord[1]:(lst$x1_coord[1]+1), lst$x1_coord[2]:(lst$x1_coord[2]+1)]))
+    lst$x1_coord[1] <- lst$x1_coord[1] + which.min(img_gray[lst$x1_coord[1]:lst$x2_coord[1], lst$x1_coord[2]]) - 1 + 2*(px_fact-(1/2))
+  if (all(img_gray[lst$y1_coord[1]:(lst$y1_coord[1]+1), lst$y1_coord[2]:(lst$y1_coord[2]+1)]))
+    lst$y1_coord[2] <- lst$y1_coord[2] - which.min(img_gray[lst$y1_coord[1], lst$y1_coord[2]:lst$y2_coord[2]]) + 2*(px_fact-(1/2))
   return(lst)
 }
 
@@ -90,7 +117,7 @@ locateYCoordinates <- function(img_gray, initAxesVec, axis_pos = "start", ax_val
     loc <- 3
   axesVec <- c()
   
-  # locate start point
+  # check the presence of the start point
   top_crop_area <- getAxisValueCropArea(img_gray, initAxesVec[1], initAxesVec[1+loc], axis = "y", section = "top", px_fact = px_fact)
   bottom_crop_area <- getAxisValueCropArea(img_gray, initAxesVec[1], initAxesVec[1+loc], axis = "y", section = "bottom", px_fact = px_fact)
   total_top_area <- dim(top_crop_area)[1]*dim(top_crop_area)[2]
@@ -133,7 +160,7 @@ locateYCoordinates <- function(img_gray, initAxesVec, axis_pos = "start", ax_val
         total_bottom_area <- dim(bottom_crop_area)[1]*dim(bottom_crop_area)[2]
         if (sum(top_crop_area)/total_top_area < ax_val_th && sum(bottom_crop_area)/total_bottom_area < ax_val_th) {
           axesVec <- c(initAxesVec[1],p_coord_point)
-          break()
+          break
         }
       }
     } else {
@@ -150,7 +177,7 @@ locateYCoordinates <- function(img_gray, initAxesVec, axis_pos = "start", ax_val
         total_bottom_area <- dim(bottom_crop_area)[1]*dim(bottom_crop_area)[2]
         if (sum(top_crop_area)/total_top_area < ax_val_th && sum(bottom_crop_area)/total_bottom_area < ax_val_th) {
           axesVec <- c(initAxesVec[1],p_coord_point)
-          break()
+          break
         }
       }
     }
@@ -161,6 +188,7 @@ locateYCoordinates <- function(img_gray, initAxesVec, axis_pos = "start", ax_val
   # if axis value couldn't be located then return initial vector
   if (any(is.na(axesVec)) || !length(axesVec) == 2)
     axesVec <- c(initAxesVec[1],initAxesVec[1+loc])
+
   return(axesVec)
 }
 
@@ -180,21 +208,25 @@ locateXCoordinates <- function(img_gray, initAxesVec, axis_pos = "start", ax_val
   
   # logic to check whether axis value is found or not
   if (sum(left_crop_area)/total_left_area > ax_val_th || sum(right_crop_area)/total_right_area > ax_val_th) {
+    
     # if axis value not found
     # logic to check ticks outside (cartesian coordinate 4)
     p_coord_points <- which(img_gray[,(initAxesVec[3]+(3*px_fact))] == FALSE)
     p_coord_points <- p_coord_points[(p_coord_points>initAxesVec[1]+(2*px_fact)) & (p_coord_points<initAxesVec[2]-(2*px_fact))]
     if (!length(p_coord_points)) {
+      
       # logic to check ticks inside (cartesian coordinate 1)
       p_coord_points <- which(img_gray[,(initAxesVec[3]-(3*px_fact))] == FALSE)
       p_coord_points <- p_coord_points[(p_coord_points>initAxesVec[1]+(2*px_fact)) & (p_coord_points<initAxesVec[2]-(2*px_fact))]
     }
     if (!length(p_coord_points))
       return(c(initAxesVec[loc],initAxesVec[3]))
+    
     # logic for finding normally distributed or logarithmic increamental peaks
     tick_chunks <- split(p_coord_points, cumsum(c(1, diff(p_coord_points) != 1)))
     p_coord_points <- unname(sapply(tick_chunks, function(x) ceiling(mean(x)))) # USE.NAMES = FALSE not working
     seq_diff <- diff(p_coord_points)
+    
     # logarithmic with increamental ticks
     if (length(unique(seq_diff)) > 7) {
       p_peak_pos <- findpeaks(seq_diff, threshold = 5)[,2]
@@ -212,10 +244,11 @@ locateXCoordinates <- function(img_gray, initAxesVec, axis_pos = "start", ax_val
         total_right_area <- dim(right_crop_area)[1]*dim(right_crop_area)[2]
         if (sum(left_crop_area)/total_left_area < ax_val_th && sum(right_crop_area)/total_right_area < ax_val_th) {
           axesVec <- c(p_coord_point,initAxesVec[3])
-          break()
+          break
         }
       }
     } else {
+      
       # linear with equally or nearly equally distributed ticks
       range <- 1:length(p_coord_points)
       if (!axis_pos == "start")
@@ -228,14 +261,16 @@ locateXCoordinates <- function(img_gray, initAxesVec, axis_pos = "start", ax_val
         total_right_area <- dim(right_crop_area)[1]*dim(right_crop_area)[2]
         if (sum(left_crop_area)/total_left_area < ax_val_th && sum(right_crop_area)/total_right_area < ax_val_th) {
           axesVec <- c(p_coord_point,initAxesVec[3])
-          break()
+          break
         }
       }
     }
   } else {
+    
     # if axis value found
     axesVec <- c(initAxesVec[loc],initAxesVec[3])
   }
+  
   # if axis value couldn't be located then return initial vector
   if (any(is.na(axesVec)) || !length(axesVec) == 2)
     axesVec <- c(initAxesVec[loc],initAxesVec[3])
@@ -257,37 +292,38 @@ getAxisValueCropArea <- function(img_gray, w, h, axis = "x", section = "top", px
   
   if (section == "top" || section == "left") {
     if (axis == "y") {
-      prox_dist_fact <- round((w/px_fact)/64)
-      prox_dist_fact <- ifelse(prox_dist_fact == 0, 1, prox_dist_fact)
+      prox_dist_fact <- ceiling((w/px_fact)/64)
+      #prox_dist_fact <- ifelse(prox_dist_fact == 0, 1, prox_dist_fact)
       width_min <- w-(20*px_fact*prox_dist_fact)
       width_max <- w-(6*px_fact*prox_dist_fact)
       height_min <- h-(3*px_fact)
       height_max <- h-px_fact
     } else if (axis == "x") {
-      prox_dist_fact <- round(((height-h)/px_fact)/64)
-      prox_dist_fact <- ifelse(prox_dist_fact == 0, 1, prox_dist_fact)
+      prox_dist_fact <- ceiling(((height-h)/px_fact)/64)
+      #prox_dist_fact <- ifelse(prox_dist_fact == 0, 1, prox_dist_fact)
       width_min <- w-(5*px_fact)
-      width_max <- w-px_fact
+      width_max <- w
       height_min <- h+(6*px_fact*prox_dist_fact)
       height_max <- h+(18*px_fact*prox_dist_fact)
     }
   } else if (section == "bottom" || section == "right") {
     if (axis == "y") {
-      prox_dist_fact <- round((w/px_fact)/64)
-      prox_dist_fact <- ifelse(prox_dist_fact == 0, 1, prox_dist_fact)
+      prox_dist_fact <- ceiling((w/px_fact)/64)
+      #prox_dist_fact <- ifelse(prox_dist_fact == 0, 1, prox_dist_fact)
       width_min <- w-(20*px_fact*prox_dist_fact)
       width_max <- w-(6*px_fact*prox_dist_fact)
       height_min <- h+px_fact
       height_max <- h+(3*px_fact)
     } else if (axis == "x") {
-      prox_dist_fact <- round(((height-h)/px_fact)/64)
-      prox_dist_fact <- ifelse(prox_dist_fact == 0, 1, prox_dist_fact)
+      prox_dist_fact <- ceiling(((height-h)/px_fact)/64)
+      #prox_dist_fact <- ifelse(prox_dist_fact == 0, 1, prox_dist_fact)
       width_min <- w+px_fact
       width_max <- w+(5*px_fact)
       height_min <- h+(6*px_fact*prox_dist_fact)
       height_max <- h+(18*px_fact*prox_dist_fact)
     }
   }
+  
   # condition to prevent values outside of image
   if (width_min < 1 || width_min > width)
     width_min <- wVec[which.min(c(width_min-wVec[1],wVec[2]-width_min))]
@@ -310,3 +346,4 @@ displayXYCoordinates <- function(img, lst) {
   img_draw <- EBImage::drawCircle(img_draw, lst$y2_coord[1], lst$y2_coord[2], 3, col = 1, fill = T)
   display(img_draw)
 }
+
